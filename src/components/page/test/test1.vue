8ba77b1029818bc69028bd3bf1bdc83ca5529ab7
haifeng520 <39614671+haifeng520@users.noreply.github.com>
2019-05-15 13:41:09 +0800

    first

---
+++ src/components/page/test/test1.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-card style="overflow: auto;">
+    <el-button @click="handleClick">click me</el-button>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  methods: {
+    handleClick() {
+      alert(1);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import Vue from "vue";
+import Router from "vue-router";
+import { Message } from "element-ui";
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: "/login",
+      component: () => import("../components/common/Login.vue")
+    },
+    {
+      path: "/",
+      component: resolve => require(["../components/common/Home.vue"], resolve),
+      meta: { title: "自述文件" },
+      children: [
+        {
+          path: "/welcome",
+          component: () => import("../components/common/welcome.vue"),
+          meta: { title: "欢迎页" }
+        },
+        {
+          path: "/classmanagement",
+          component: () =>
+            import("../components/page/customerService/ClassManagement.vue"),
+          meta: { title: "班级管理" }
+        },
+        {
+          path: "/classmanagement/add",
+          component: () =>
+            import("../components/page/customerService/ClassManagementAdd.vue")
+        },
+        {
+          path: "/classmanagement/edit",
+          name: "editClass", // 组件传值 路由名称
+          component: () =>
+            import("../components/page/customerService/ClassManagementEdit.vue")
+        },
+        {
+          path: "/test",
+          component: () => import("../components/page/test/test.vue"),
+          meta: { title: "测试" }
+        },
+        {
+          path: "/editor",
+          component: () =>
+            import("../components/page/test/vue-quill-editor.vue"),
+          meta: { title: "富文本" }
+        }
+      ]
+    }
+  ]
+});
+
+// 设置全局的前置路由守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login") {
+    next();
+  } else {
+    // 判断有没有 token
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      next();
+    } else {
+      router.push("login");
+      // 提示一下
+      Message.warning("请先登录");
+    }
+  }
+});
+
+export default router;
